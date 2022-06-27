c5ac2df9fbc26af97792018b4d5e83528138bba6
zjyCA <jy518jy@gmail.com>
2022-06-26 20:57:44 -0400

    first

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let circularProgress = document.querySelector('.circular-progress'),
+    progressValue = document.querySelector('.progress-value')
+
+let progressStartValue = 0,
+progressEndValue = 100,
+speed = 100
+
+let progress = setInterval(() => {
+    progressStartValue++
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#7d2a7d ${progressStartValue * 3.6}deg,#ededed 0deg)`
+    if (progressStartValue == progressEndValue){
+        clearInterval(progress)
+    }
+    console.log(progressStartValue)
+}, speed);
